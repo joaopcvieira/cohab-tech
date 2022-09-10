9a6e3ecccf34db3ccd6d00caba38f1096b97de71
Joao Vieira <joaopcvieira962@gmail.com>
2022-09-10 14:13:08 -0300

    add: files

--- email-sender.py
+++ email-sender.py
@@ -1,6 +1,43 @@
-from  templates import *
-from  email.mime.multipart  import  MIMEMultipart
-from  email.mime.text  import  MIMEText
-import  smtplib
+import pandas as pd
+import gspread
+# from templates import *
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
 
-print(dever_boletos)
+sa = gspread.service_account(filename='credentials.json')
+sh = sa.open("Home Broker")
+ws = sh.worksheet("alunos")
+
+print(ws.get("A1:K1")[0])
+
+
+atributos = ['nome',
+             'apelido',
+             'turma',
+             'cpf',
+             'email',
+             'ap-bloco', 'ap-numero', 'ap-vaga',
+             'pontos-total', 'pontos-presenca', 'pontos-boletos',
+             'pontos-iniciativas-I1-nome', 'pontos-iniciativas-I1-pontos',
+             'pontos-iniciativas-I2-nome', 'pontos-iniciativas-I2-pontos',
+             'pontos-iniciativas-I3-nome', 'pontos-iniciativas-I3-pontos',
+             ]
+alunos = pd.DataFrame(columns=atributos, data=ws.get("A2:Q100"))
+
+print(alunos.head())
+
+
+# for index, aluno in alunos.iterrows():
+#     print(aluno['nome'])
+#     print(aluno['email'])
+#     print(aluno['pontos-total'])
+#     print(aluno['pontos-presenca'])
+#     print(aluno['pontos-boletos'])
+#     print(aluno['pontos-iniciativas-I1-nome'])
+#     print(aluno['pontos-iniciativas-I1-pontos'])
+#     print(aluno['pontos-iniciativas-I2-nome'])
+#     print(aluno['pontos-iniciativas-I2-pontos'])
+#     print(aluno['pontos-iniciativas-I3-nome'])
+#     print(aluno['pontos-iniciativas-I3-pontos'])
+#     print()
--- templates.py
+++ templates.py
@@ -1,12 +1,127 @@
-dever_boletos = '''<!DOCTYPE html>
+# aluno = {
+#     'nome': 'João Pedro',
+#     'apelido': 'João',
+#     'turma': 'T25',
+#     'cpf': '123.456.789-00',
+#     'email': 'meuemail@gmail.com',
+#     'ap': {
+#         'numero': '123',
+#         'bloco': 'A',
+#         'vaga': 'D'
+#     },
+#     'pontos': {
+#         'total': 10,
+#         'presenca': 0,
+#         'boletos': 0,
+#         'iniciativas': {
+#             'I1': {
+#                 'nome': 'Iniciativa 1',
+#                 'pontos': 0
+#             },
+#             'I2': {
+#                 'nome': 'Iniciativa 2',
+#                 'pontos': 0                
+#             },
+#             'I3': {
+#                 'nome': 'Iniciativa 3',
+#                 'pontos': 0
+#             },
+#         }
+#     }
+# }
+
+def msg_validacao_pontos(linha):
+    validacao_pontos = '''
+<!DOCTYPE html>
+<html>
+  <head>
+    <base target="_top">
+  </head>
+  <body>
+   
+    <h3>Validação dos pontos</h3>
+    
+    <h4>Olá, {linha.nome}! </h4>
+'''
+
+#     <p>Seus pontos foram validados com sucesso! Você apresentou no final, um total de <i>{aluno['pontos']['total']}.</i></p>
+#     <br>
+#     <p>Segue abaixo a divisão e origem dessa pontuação:</p>
+#     <br> '''
+    
+#     if(aluno['pontos']['presenca']):
+#         validacao_pontos += '''
+#     <p>Presença em AG's: {aluno['pontos']['presenca']}</p>'''
+    
+#     if(aluno['pontos']['boletos']):  
+#         validacao_pontos += '''
+#     <p>Boletos pagos: {aluno['pontos']['boletos']}</p>'''
+
+#     if(aluno['pontos']['iniciativas']['I1']['pontos']):
+#         validacao_pontos += '''
+#     <h4>Iniciativas </h4>
+#     <p> {aluno['pontos']['iniciativas']['I1']['nome']} <br/>
+#     Correspondentes a um total de {aluno['pontos']['iniciativas']['I1']['pontos']} pontos. </p>'''
+
+#     if(aluno['pontos']['iniciativas']['I2']['pontos']):
+#         validacao_pontos += '''
+#     <p> {aluno['pontos']['iniciativas']['I2']['nome']} <br/>
+#     Correspondentes a um total de {aluno['pontos']['iniciativas']['I2']['pontos']} pontos. </p>'''
+
+#     if(aluno['pontos']['iniciativas']['I3']['pontos']):
+#         validacao_pontos += '''
+#     <p> {aluno['pontos']['iniciativas']['I3']['nome']} <br/>
+#     Correspondentes a um total de {aluno['pontos']['iniciativas']['I3']['pontos']} pontos. </p>'''
+
+
+#     validacao_pontos +='''
+#     <p>Qualquer dúvida ou sugestão a respeito da validação dos pontos, estamos à disposição,</p>
+
+#     <p>Isabel, Begis T-25<br/>
+#     Diretora da Cohab.</p>
+    
+#   </body>
+# </html>
+# '''
+    return validacao_pontos.format(linha=linha)
+
+def msg_resultado_alocacao(linha):
+    resultado_alocacao = '''
+<!DOCTYPE html>
+<html>
+<head>
+    <base target = "_top">
+</head>
+<body>
+
+    <h3>Resultado da Postulação</h3>
+
+    <h4>Parabéns, {aluno.name}!</h4>
+
+    <p>Você conseguiu postular para a vaga {aluno.ap.vaga}, do apartamento {aluno.ap.numero}!</p>
+
+    <p> Qualquer dúvida ou sugestão, estamos à disposição,</p>
+    <p> Isabel Alencar Tavares Colares Brasil, <br/>
+        Diretora da CoHab.</p>
+    
+
+</body>
+</html> '''
+    return resultado_alocacao.format(linha=linha)
+
+def msg_dever_boletos(linha):
+    dever_boletos = '''
+<!DOCTYPE html>
 <html>
 	<head>
 		<base target="_top">
 	</head>
 	<body>
-		<h4>Olá, {NOME}</h4>
+		<h4>Olá, {nome}</h4>
 
-		<p>No tocante ao corrente processo de postulação, recebemos o seu pedido de postulação referente às vagas {VAGAS}<!-- $vagas, talvez listadas ... usar um concat e show -->.</p>
+		<p>No tocante ao corrente processo de postulação, recebemos o seu pedido de postulação referente às vagas {VAGAS}
+        <!-- $vagas, talvez listadas ... usar um concat e show -->
+        .</p>
 
 		<p>Entretanto, notamos aqui que você está com pendências em relação ao pagamento da taxa de manutenção do H8, e isso configura um impedimento para a sua participação no processo de escolha de vagas.</p>
 
@@ -30,3 +145,9 @@ dever_boletos = '''<!DOCTYPE html>
 	</body>
 </html>'''
 # .format(NOME=..., VAGAS=..., MESES=...)
+
+
+# print(msg_validacao_pontos(aluno))
+
+    print('teste de {nome}'.format(**linha))
+    return msg_validacao_pontos(linha)
