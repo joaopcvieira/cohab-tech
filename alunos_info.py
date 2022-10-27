76ee10bf82e289677cc34de8ceee2688bd18f155
Joao Vieira <joaopcvieira962@gmail.com>
2022-10-27 11:17:09 -0300

    update alunos info

--- alunos_info.py
+++ alunos_info.py
@@ -16,8 +16,40 @@ def remove_accents_and_special_characters(word):
         # Remove anything that is not a word or digit
         pattern=r'[^a-zA-Z0-9\s]', repl='', string=word
     )
-
     return word
+
+def get_pontos_total(alunos: pd.DataFrame) -> pd.DataFrame:
+    pontos_total = []
+    for idx, linha in alunos.iterrows():
+        pt = 2
+        try:
+            pt += float(linha.pontos_antigos)
+        except:
+            pass
+        try:
+            pt += float(linha.pontos_boletos)
+        except:
+            pass
+        try:
+            pt += float(linha.pontos_presenca)
+        except:
+            pass
+        try:
+            pt += float(linha.pontos_iniciativas_I1_pontos)
+        except:
+            pass
+        try:
+            pt += float(linha.pontos_iniciativas_I2_pontos)
+        except:
+            pass
+        try:
+            pt += float(linha.pontos_iniciativas_I3_pontos)
+        except:
+            pass
+
+        pontos_total.append(pt)
+    return pontos_total
+
 
 def get_students_info() -> pd.DataFrame:
     sa = gspread.service_account(filename='credentials.json')
@@ -27,67 +59,45 @@ def get_students_info() -> pd.DataFrame:
     planilha_AG = sa.open('Presença na AG').worksheet("Geral")
 
     atributos = ['nome',
-                'apelido',
-                'turma',
-                'cpf',
-                'celular', 'email',
-                'ap_bloco', 'ap_numero', 'ap_vaga',
-                'pontos_total', 'pontos_antigos', 'pontos_presenca', 'pontos_boletos',
-                'pontos_iniciativas_I1_nome', 'pontos_iniciativas_I1_pontos',
-                'pontos_iniciativas_I2_nome', 'pontos_iniciativas_I2_pontos',
-                'pontos_iniciativas_I3_nome', 'pontos_iniciativas_I3_pontos',
-                ]
+                 'apelido',
+                 'turma',
+                 'cpf',
+                 'celular', 'email',
+                 'ap_bloco', 'ap_numero', 'ap_vaga',
+                 'pontos_total', 'pontos_antigos', 'pontos_presenca', 'pontos_boletos',
+                 'pontos_iniciativas_I1_nome', 'pontos_iniciativas_I1_pontos',
+                 'pontos_iniciativas_I2_nome', 'pontos_iniciativas_I2_pontos',
+                 'pontos_iniciativas_I3_nome', 'pontos_iniciativas_I3_pontos',
+                 ]
     alunos = pd.DataFrame(columns=atributos, data=ws.get("A2:S"))
-    
-   
-    nomes_alunos_presentes = alunos_presentes_nas_ags().nome.apply(remove_accents_and_special_characters).values
-    alunos['pontos_presenca'] = [1 if nome in nomes_alunos_presentes else 0 for nome in alunos.nome.apply(remove_accents_and_special_characters).values]
 
-    alunos_devendo_boletos = boletos_infos().nome.apply(remove_accents_and_special_characters).values
-    alunos['meses_devendo'] = [devendo if nome in alunos_devendo_boletos else '' for devendo, nome in zip(boletos_infos().meses_devendo.values, alunos.nome.apply(remove_accents_and_special_characters).values)]
+    # Analisando os pontos de presença
+    nomes_alunos_presentes = alunos_presentes_nas_ags().nome.apply(
+        remove_accents_and_special_characters).values
+    alunos['pontos_presenca'] = [1 if nome in nomes_alunos_presentes else
+                                 0 for nome in alunos.nome.apply(
+                                     remove_accents_and_special_characters).values]
 
-    alunos['pontos_boletos'] = [0.5 if devendo == '' else 0 for devendo in alunos.meses_devendo.values]
+    # Analisando os pontos de boletos
+    boletos_df = boletos_infos()
+    # Criando uma coluna temporária para a junção dos df
+    boletos_df['nome_join'] = boletos_df.nome.apply(
+        remove_accents_and_special_characters)
+    alunos['nome_join'] = alunos.nome.apply(
+        remove_accents_and_special_characters)
 
+    alunos = pd.merge(
+        alunos, boletos_df[['nome_join', 'meses_devendo']], on='nome_join', how='left')
+    alunos.drop(columns=['nome_join'], inplace=True)
 
-    pontos_total = []
-    for idx, linha in alunos.iterrows():
-        pt = 2
-        # if linha.pontos_antigos != '': # or linha.pontos_antigos.notnull():
-        try:
-            pt += float(linha.pontos_antigos)
-        except:
-            pass
-        # if linha.pontos_boletos != '': # or linha.pontos_boletos.notnull():
-        try:
-            pt += float(linha.pontos_boletos)
-        except:
-            pass
-        # if linha.pontos_presenca != '': # or linha.pontos_presenca.notnull():
-        try:
-            pt += float(linha.pontos_presenca)
-        except:
-            pass
-        # if linha.pontos_iniciativas_I1_pontos != '': # or linha.pontos_iniciativas_I1_pontos.notnull():
-        try:
-            pt += float(linha.pontos_iniciativas_I1_pontos)
-        except:
-            pass
-        # if linha.pontos_iniciativas_I2_pontos != '': # or linha.pontos_iniciativas_I2_pontos.notnull():
-        try:
-            pt += float(linha.pontos_iniciativas_I2_pontos)
-        except:
-            pass
-        # if linha.pontos_iniciativas_I3_pontos != '': # or linha.pontos_iniciativas_I3_pontos.notnull():
-        try:
-            pt += float(linha.pontos_iniciativas_I3_pontos)
-        except:
-            pass
+    alunos = alunos.fillna('-')
+    alunos['pontos_boletos'] = [0.5 if devendo == '-' else 0 for devendo in alunos.meses_devendo.values]
 
-        pontos_total.append(pt)
-    
-    alunos['pontos_total'] = pontos_total
+    alunos['pontos_total'] = get_pontos_total(alunos)
+
+    ws_export = sh.worksheet("import_python")
+    ws_export.update([alunos.columns.values.tolist()] + alunos.values.tolist())
     return alunos
 
-# print(get_students_info().columns)
-
-
+# df = get_students_info()
+# print(df)
