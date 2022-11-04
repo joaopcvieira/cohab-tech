c7e7c5807dc3e595f85d817c5231e669bc7ec344
Joao Vieira <joaopcvieira962@gmail.com>
2022-11-04 17:47:35 -0300

    version for sending emails on sheets

--- index.py
+++ index.py
@@ -1,38 +1,54 @@
 from templates import *
+import numpy as np
 
-from send_email import send_email
-from api_wpp import send_message
-from chat_bot_Selenium import send_messages_selenium
+# from send_email import send_email
+# from api_wpp import send_message
+# from chat_bot_Selenium import send_messages_selenium
+import chat_bot_Selenium as selenium
+import twilio_sender as twilio
 
-from alunos_info import get_students_info
+import alunos_info
 from boletos_infos import boletos_infos
 
 
 def main():
-    alunos = get_students_info()
+    alunos = alunos_info.get_students_info()
     alunos['mnsg_enviada'] = False
     devendo_boletos = boletos_infos()
 
+    mensagens = list()
     for idx, linha in alunos.iterrows():
-        if idx == 0:
-            pass
-
-        elif linha.meses_devendo != '':
-            # print(msg_dever_boletos(linha))
-            send_message(msg_dever_boletos(linha), f'{linha.celular}')
-            linha.update({'mnsg_enviada': True})
-            break
+        if linha.meses_devendo != '-':
+            mensagens.append(msg_dever_boletos_sheets(linha))
+           
+            # try:
+            #     print(f'O telefone foi {linha.celular}')
+            #     twilio.send_message(msg_dever_boletos(linha), f'{linha.celular}')
+            #     print(f'Mensagem enviada para {linha.nome}')
+            #     linha.update({'mnsg_enviada': True})
+            # except:
+            #     print(f'Erro ao enviar mensagem para {linha.nome}. Ele DEVE boletos.')
+            # break
 
         else:
-            # send_email(linha.email, msg_validacao_pontos(linha))
-            send_message(msg_validacao_pontos(linha), f'{linha.celular}')
-            send_message(msg_resultado_alocacao(linha), f'{linha.celular}')
-            print("Mnsg enviada para ", linha.nome)
-            alunos.loc[idx, 'mnsg_enviada'] = True
+            mensagens.append("")
+            # mensagens.append(msg_validacao_pontos_sheets(linha))
+            
+            # # send_email(linha.email, msg_validacao_pontos(linha))
+            # print(f'O telefone foi {linha.celular}')
+            # twilio.send_message(msg_validacao_pontos(linha), f'{linha.celular}')
+            # # send_message(msg_resultado_alocacao(linha), f'{linha.celular}')
+            # # print("Mnsg enviada para ", linha.nome)
+            # alunos.loc[idx, 'mnsg_enviada'] = True
+    alunos_info.update_mnsg_sheets_manual_check(np.array([mensagens]))
 
-        # if idx == 1:
-        #     break
 
+    devendo = alunos[alunos.meses_devendo != '-']
+    devendo['mensagens'] = devendo.apply(lambda x: msg_dever_boletos_wpp(x), axis=1)
+    # selenium.send_messages_selenium(devendo)
+
+        
+        
 
 if __name__ == '__main__':
     main()
